f746f20d4375a2a97bf2d9d5c4446f4601d16277
valerialeiva <Suisivale30>
2020-11-07 12:14:01 -0600

    Datos de prueba

--- Datos de prueba.sql
+++ Datos de prueba.sql
@@ -124,3 +124,175 @@ GO
 INSERT INTO dbo.TMCCM_Grupo_Vehiculo(TN_ID_Vehiculo,TN_ID_Grupo,TN_Km_Inicio,TN_Km_Regreso,TF_Fecha_Hora)
      VALUES(0,0,0,null,'')
 GO
+
+--C Ubicacion Tipo Ubicacion
+SET IDENTITY_INSERT [dbo].[TMCCM_C_Ubicacion_Tipo_Ubicacion] ON 
+
+INSERT [dbo].[TMCCM_C_Ubicacion_Tipo_Ubicacion] ([TN_ID_Tipo_Ubicacion], [TC_Nombre], [TC_Descripcion], [TB_Eliminado]) VALUES (6, N'Casa', N'Construcción cubierta destinada a ser habitada', 0)
+INSERT [dbo].[TMCCM_C_Ubicacion_Tipo_Ubicacion] ([TN_ID_Tipo_Ubicacion], [TC_Nombre], [TC_Descripcion], [TB_Eliminado]) VALUES (7, N'Parque', N'Terreno acotado en núcleos rurales o urbanos, generalmente con plantas y árboles', 0)
+INSERT [dbo].[TMCCM_C_Ubicacion_Tipo_Ubicacion] ([TN_ID_Tipo_Ubicacion], [TC_Nombre], [TC_Descripcion], [TB_Eliminado]) VALUES (8, N'Edificio', N'Construcción de grandes dimensiones fabricada con piedras, ladrillos y materiales resistentes', 0)
+SET IDENTITY_INSERT [dbo].[TMCCM_C_Ubicacion_Tipo_Ubicacion] OFF
+GO
+
+--C Ubicacion Provincia
+SET IDENTITY_INSERT [dbo].[TMCCM_C_Ubicacion_Provincia] ON 
+
+INSERT [dbo].[TMCCM_C_Ubicacion_Provincia] ([TN_ID_Provincia], [TF_Fecha_Creacion], [TC_Descripcion], [TB_Eliminado]) VALUES (1, CAST(N'2020-10-21T00:00:00.000' AS DateTime), N'NO INDICA', 0)
+INSERT [dbo].[TMCCM_C_Ubicacion_Provincia] ([TN_ID_Provincia], [TF_Fecha_Creacion], [TC_Descripcion], [TB_Eliminado]) VALUES (2, CAST(N'2020-10-21T00:00:00.000' AS DateTime), N'SAN JOSÉ', 0)
+INSERT [dbo].[TMCCM_C_Ubicacion_Provincia] ([TN_ID_Provincia], [TF_Fecha_Creacion], [TC_Descripcion], [TB_Eliminado]) VALUES (3, CAST(N'2020-10-21T00:00:00.000' AS DateTime), N'CARTAGO', 0)
+SET IDENTITY_INSERT [dbo].[TMCCM_C_Ubicacion_Provincia] OFF
+GO
+
+--C Ubicacion Cantón
+SET IDENTITY_INSERT [dbo].[TMCCM_C_Ubicacion_Canton] ON 
+
+INSERT [dbo].[TMCCM_C_Ubicacion_Canton] ([TN_ID_Canton], [TN_ID_Provincia], [TF_Fecha_Creacion], [TC_Descripcion], [TB_Eliminado]) VALUES (1, 1, CAST(N'2020-10-21T00:00:00.000' AS DateTime), N'NO INDICA', 0)
+INSERT [dbo].[TMCCM_C_Ubicacion_Canton] ([TN_ID_Canton], [TN_ID_Provincia], [TF_Fecha_Creacion], [TC_Descripcion], [TB_Eliminado]) VALUES (2, 2, CAST(N'2020-10-21T00:00:00.000' AS DateTime), N'SAN JOSÉ', 0)
+INSERT [dbo].[TMCCM_C_Ubicacion_Canton] ([TN_ID_Canton], [TN_ID_Provincia], [TF_Fecha_Creacion], [TC_Descripcion], [TB_Eliminado]) VALUES (3, 3, CAST(N'2020-10-21T00:00:00.000' AS DateTime), N'PARAÍSO', 0)
+SET IDENTITY_INSERT [dbo].[TMCCM_C_Ubicacion_Canton] OFF
+GO
+
+--Ubicación Distrito
+SET IDENTITY_INSERT [dbo].[TMCCM_C_Ubicacion_Distrito] ON 
+
+INSERT [dbo].[TMCCM_C_Ubicacion_Distrito] ([TN_ID_Distrito], [TN_ID_Canton], [TF_Fecha_Creacion], [TC_Descripcion], [TB_Eliminado]) VALUES (1, 1, CAST(N'2020-10-21T00:00:00.000' AS DateTime), N'NO INDICA', 0)
+INSERT [dbo].[TMCCM_C_Ubicacion_Distrito] ([TN_ID_Distrito], [TN_ID_Canton], [TF_Fecha_Creacion], [TC_Descripcion], [TB_Eliminado]) VALUES (2, 2, CAST(N'2020-10-21T00:00:00.000' AS DateTime), N'NO INDICA', 0)
+INSERT [dbo].[TMCCM_C_Ubicacion_Distrito] ([TN_ID_Distrito], [TN_ID_Canton], [TF_Fecha_Creacion], [TC_Descripcion], [TB_Eliminado]) VALUES (3, 3, CAST(N'2020-10-21T00:00:00.000' AS DateTime), N'NO INDICA', 0)
+SET IDENTITY_INSERT [dbo].[TMCCM_C_Ubicacion_Distrito] OFF
+GO
+
+--Entidad Ubicación
+SET IDENTITY_INSERT [dbo].[TMCCM_Entidad_Ubicacion] ON 
+
+INSERT [dbo].[TMCCM_Entidad_Ubicacion] ([TN_ID_Ubicacion], [TN_ID_Caso], [TN_ID_Tipo_Ubicacion], [TN_ID_Provincia], [TN_ID_Canton], [TN_ID_Distrito], [TC_Sennas], [TD_Latitud], [TD_Longitud], [TF_Fecha_Creacion], [TF_Fecha_Modificacion], [TC_Creado_Por], [TC_Modificado_Por], [TB_Eliminado], [TB_Verificado]) VALUES (21, 15, 6, 1, 1, 1, N'200 metros la norte de templo católico', 1080, 950, CAST(N'2020-11-06T00:00:00.000' AS DateTime), CAST(N'2020-11-06T00:00:00.000' AS DateTime), N'Valeria Leiva Quirós', N'Valeria Leiva Quirós', 0, 1)
+INSERT [dbo].[TMCCM_Entidad_Ubicacion] ([TN_ID_Ubicacion], [TN_ID_Caso], [TN_ID_Tipo_Ubicacion], [TN_ID_Provincia], [TN_ID_Canton], [TN_ID_Distrito], [TC_Sennas], [TD_Latitud], [TD_Longitud], [TF_Fecha_Creacion], [TF_Fecha_Modificacion], [TC_Creado_Por], [TC_Modificado_Por], [TB_Eliminado], [TB_Verificado]) VALUES (22, 15, 7, 2, 2, 2, N'Al frente del Teatro Nacional de Costa Rica', 1080, 200, CAST(N'2020-11-06T00:00:00.000' AS DateTime), CAST(N'2020-11-06T00:00:00.000' AS DateTime), N'Valeria Leiva Quirós', N'Valeria Leiva Quirós', 0, 1)
+INSERT [dbo].[TMCCM_Entidad_Ubicacion] ([TN_ID_Ubicacion], [TN_ID_Caso], [TN_ID_Tipo_Ubicacion], [TN_ID_Provincia], [TN_ID_Canton], [TN_ID_Distrito], [TC_Sennas], [TD_Latitud], [TD_Longitud], [TF_Fecha_Creacion], [TF_Fecha_Modificacion], [TC_Creado_Por], [TC_Modificado_Por], [TB_Eliminado], [TB_Verificado]) VALUES (23, 15, 8, 3, 3, 3, N'500 metros oeste de la estación de bomberos', 1080, 800, CAST(N'2020-11-06T00:00:00.000' AS DateTime), NULL, N'Valeria Leiva Quirós', NULL, 0, 0)
+INSERT [dbo].[TMCCM_Entidad_Ubicacion] ([TN_ID_Ubicacion], [TN_ID_Caso], [TN_ID_Tipo_Ubicacion], [TN_ID_Provincia], [TN_ID_Canton], [TN_ID_Distrito], [TC_Sennas], [TD_Latitud], [TD_Longitud], [TF_Fecha_Creacion], [TF_Fecha_Modificacion], [TC_Creado_Por], [TC_Modificado_Por], [TB_Eliminado], [TB_Verificado]) VALUES (25, 6, 7, 3, 3, 3, N'Al oeste de la pizzería el Forno', 1050, 1000, CAST(N'2020-11-06T00:00:00.000' AS DateTime), CAST(N'2020-11-06T00:00:00.000' AS DateTime), N'Valeria Leiva Quirós', N'Valeria Leiva Quirós', 0, 1)
+SET IDENTITY_INSERT [dbo].[TMCCM_Entidad_Ubicacion] OFF
+GO
+--C_Droga Tipo Droga
+
+SET IDENTITY_INSERT [dbo].[TMCCM_C_Droga_Tipo_Droga] ON 
+
+INSERT [dbo].[TMCCM_C_Droga_Tipo_Droga] ([TN_ID_Tipo_Droga], [TF_Fecha_Creacion], [TC_Nombre], [TC_Descripcion], [TB_Eliminado]) VALUES (21, CAST(N'2020-11-06T21:36:01.190' AS DateTime), N'Alcohol', N'Bebida que contiene alcohol etílico', 0)
+INSERT [dbo].[TMCCM_C_Droga_Tipo_Droga] ([TN_ID_Tipo_Droga], [TF_Fecha_Creacion], [TC_Nombre], [TC_Descripcion], [TB_Eliminado]) VALUES (22, CAST(N'2020-11-06T21:36:21.330' AS DateTime), N'Tabaco', N'Hoja de esta planta que, curada y preparada, se fuma, se masca o se aspira en forma de rapé', 0)
+INSERT [dbo].[TMCCM_C_Droga_Tipo_Droga] ([TN_ID_Tipo_Droga], [TF_Fecha_Creacion], [TC_Nombre], [TC_Descripcion], [TB_Eliminado]) VALUES (23, CAST(N'2020-11-06T21:36:45.627' AS DateTime), N'Marihuana', N'Una sustancia química que proviene de una planta llamada cannabis', 0)
+SET IDENTITY_INSERT [dbo].[TMCCM_C_Droga_Tipo_Droga] OFF
+GO
+
+--Entidad Droga
+SET IDENTITY_INSERT [dbo].[TMCCM_Entidad_Droga] ON 
+
+INSERT [dbo].[TMCCM_Entidad_Droga] ([TN_ID_Droga], [TN_ID_Caso], [TN_ID_Tipo_Droga], [TC_Nombre], [TC_Detalle], [TN_Cantidad], [TF_Fecha_Decomiso], [TB_Eliminado], [TF_Fecha_Creacion], [TF_Fecha_Modificacion], [TC_Creado_Por], [TC_Modificado_Por], [TB_Verificado]) VALUES (37, 6, 23, N'Skunk', N'Marihuana popular', 10000, CAST(N'2020-10-28T00:00:00.000' AS DateTime), 0, CAST(N'2020-11-06T21:37:55.697' AS DateTime), CAST(N'2020-11-06T21:43:07.793' AS DateTime), N'Valeria Leiva Quirós', N'Valeria Leiva Quirós', 1)
+INSERT [dbo].[TMCCM_Entidad_Droga] ([TN_ID_Droga], [TN_ID_Caso], [TN_ID_Tipo_Droga], [TC_Nombre], [TC_Detalle], [TN_Cantidad], [TF_Fecha_Decomiso], [TB_Eliminado], [TF_Fecha_Creacion], [TF_Fecha_Modificacion], [TC_Creado_Por], [TC_Modificado_Por], [TB_Verificado]) VALUES (38, 6, 22, N'Belmont ', N'Hallados en el caso', 1, CAST(N'2020-10-10T00:00:00.000' AS DateTime), 0, CAST(N'2020-11-06T21:39:00.150' AS DateTime), NULL, N'Valeria Leiva Quirós', NULL, 0)
+INSERT [dbo].[TMCCM_Entidad_Droga] ([TN_ID_Droga], [TN_ID_Caso], [TN_ID_Tipo_Droga], [TC_Nombre], [TC_Detalle], [TN_Cantidad], [TF_Fecha_Decomiso], [TB_Eliminado], [TF_Fecha_Creacion], [TF_Fecha_Modificacion], [TC_Creado_Por], [TC_Modificado_Por], [TB_Verificado]) VALUES (39, 6, 21, N'Tamborito', N'Con metanol', 100, CAST(N'2020-10-08T00:00:00.000' AS DateTime), 0, CAST(N'2020-11-06T21:39:38.217' AS DateTime), NULL, N'Valeria Leiva Quirós', NULL, 0)
+SET IDENTITY_INSERT [dbo].[TMCCM_Entidad_Droga] OFF
+GO
+
+--C_Arma Marca
+SET IDENTITY_INSERT [dbo].[TMCCM_C_Arma_Marca] ON 
+
+INSERT [dbo].[TMCCM_C_Arma_Marca] ([TN_ID_Marca_Arma], [TC_Descripcion], [TB_Eliminado]) VALUES (12, N'Desconocido', 0)
+INSERT [dbo].[TMCCM_C_Arma_Marca] ([TN_ID_Marca_Arma], [TC_Descripcion], [TB_Eliminado]) VALUES (13, N'A.A. Brown &amp; Sons', 0)
+INSERT [dbo].[TMCCM_C_Arma_Marca] ([TN_ID_Marca_Arma], [TC_Descripcion], [TB_Eliminado]) VALUES (14, N'Accu_Tek', 0)
+SET IDENTITY_INSERT [dbo].[TMCCM_C_Arma_Marca] OFF
+GO
+
+--C_Arma Tipo Arma
+SET IDENTITY_INSERT [dbo].[TMCCM_C_Arma_Tipo_Arma] ON 
+
+INSERT [dbo].[TMCCM_C_Arma_Tipo_Arma] ([TN_ID_Tipo_Arma], [TC_Descripcion], [TB_Eliminado], [TN_Peso]) VALUES (6, N'Arma Blanca', 0, NULL)
+INSERT [dbo].[TMCCM_C_Arma_Tipo_Arma] ([TN_ID_Tipo_Arma], [TC_Descripcion], [TB_Eliminado], [TN_Peso]) VALUES (7, N'Arma Fuego', 0, NULL)
+INSERT [dbo].[TMCCM_C_Arma_Tipo_Arma] ([TN_ID_Tipo_Arma], [TC_Descripcion], [TB_Eliminado], [TN_Peso]) VALUES (8, N'Arma Contundente', 0, NULL)
+SET IDENTITY_INSERT [dbo].[TMCCM_C_Arma_Tipo_Arma] OFF
+GO
+
+--Entidad Arma
+SET IDENTITY_INSERT [dbo].[TMCCM_Entidad_Arma] ON 
+
+INSERT [dbo].[TMCCM_Entidad_Arma] ([TN_ID_Arma], [TN_ID_Caso], [TN_ID_Marca_Arma], [TN_ID_Tipo_Arma], [TC_Modelo_Arma], [TC_Calibre], [TC_Serie], [TF_Fecha_Creacion], [TF_Fecha_Modificacion], [TC_Creado_Por], [TC_Modificado_Por], [TB_Eliminado], [TB_Verificado], [TC_Comentario]) VALUES (9, 6, 12, 7, N'85 Auto', N'9 mm', N'17D00349', CAST(N'2020-11-06T00:00:00.000' AS DateTime), NULL, N'Valeria Leiva Quirós', NULL, 0, 1, N'Arma hallada en el lugar')
+INSERT [dbo].[TMCCM_Entidad_Arma] ([TN_ID_Arma], [TN_ID_Caso], [TN_ID_Marca_Arma], [TN_ID_Tipo_Arma], [TC_Modelo_Arma], [TC_Calibre], [TC_Serie], [TF_Fecha_Creacion], [TF_Fecha_Modificacion], [TC_Creado_Por], [TC_Modificado_Por], [TB_Eliminado], [TB_Verificado], [TC_Comentario]) VALUES (10, 6, 13, 8, N'97 Auto', N'N.A', N'18D00348', CAST(N'2020-11-06T00:00:00.000' AS DateTime), CAST(N'2020-11-06T00:00:00.000' AS DateTime), N'Valeria Leiva Quirós', N'Valeria Leiva Quirós', 0, 0, N'Arma blanca con sangre')
+INSERT [dbo].[TMCCM_Entidad_Arma] ([TN_ID_Arma], [TN_ID_Caso], [TN_ID_Marca_Arma], [TN_ID_Tipo_Arma], [TC_Modelo_Arma], [TC_Calibre], [TC_Serie], [TF_Fecha_Creacion], [TF_Fecha_Modificacion], [TC_Creado_Por], [TC_Modificado_Por], [TB_Eliminado], [TB_Verificado], [TC_Comentario]) VALUES (11, 43, 13, 8, N'120 Auto', N'22 mm', N'000203la', CAST(N'2020-11-06T00:00:00.000' AS DateTime), NULL, N'Valeria Leiva Quirós', NULL, 1, 0, NULL)
+INSERT [dbo].[TMCCM_Entidad_Arma] ([TN_ID_Arma], [TN_ID_Caso], [TN_ID_Marca_Arma], [TN_ID_Tipo_Arma], [TC_Modelo_Arma], [TC_Calibre], [TC_Serie], [TF_Fecha_Creacion], [TF_Fecha_Modificacion], [TC_Creado_Por], [TC_Modificado_Por], [TB_Eliminado], [TB_Verificado], [TC_Comentario]) VALUES (12, 6, 12, 6, N'97 Auto', N'N.A', N'18D00325', CAST(N'2020-11-06T00:00:00.000' AS DateTime), NULL, N'Valeria Leiva Quirós', NULL, 0, 1, NULL)
+INSERT [dbo].[TMCCM_Entidad_Arma] ([TN_ID_Arma], [TN_ID_Caso], [TN_ID_Marca_Arma], [TN_ID_Tipo_Arma], [TC_Modelo_Arma], [TC_Calibre], [TC_Serie], [TF_Fecha_Creacion], [TF_Fecha_Modificacion], [TC_Creado_Por], [TC_Modificado_Por], [TB_Eliminado], [TB_Verificado], [TC_Comentario]) VALUES (13, 6, 13, 6, N'1', N'1', N'1', CAST(N'2020-11-06T00:00:00.000' AS DateTime), NULL, N'1', NULL, 1, 1, N'1')
+SET IDENTITY_INSERT [dbo].[TMCCM_Entidad_Arma] OFF
+GO
+--C_Persona Genero
+ SET IDENTITY_INSERT [dbo].[TMCCM_C_Persona_Genero] ON 
+INSERT [dbo].[TMCCM_C_Persona_Genero] ([TN_ID_Genero], [TC_Descripcion], [TB_Eliminado]) VALUES (8, N'Desconocido', 0)
+INSERT [dbo].[TMCCM_C_Persona_Genero] ([TN_ID_Genero], [TC_Descripcion], [TB_Eliminado]) VALUES (9, N'Indefinido', 0)
+INSERT [dbo].[TMCCM_C_Persona_Genero] ([TN_ID_Genero], [TC_Descripcion], [TB_Eliminado]) VALUES (10, N'Masculino', 0)
+INSERT [dbo].[TMCCM_C_Persona_Genero] ([TN_ID_Genero], [TC_Descripcion], [TB_Eliminado]) VALUES (11, N'Femenino', 0)
+SET IDENTITY_INSERT [dbo].[TMCCM_C_Persona_Genero] OFF
+GO
+
+-- C_Persona Nacionalidad
+SET IDENTITY_INSERT [dbo].[TMCCM_C_Persona_Nacionalidad] ON 
+
+INSERT [dbo].[TMCCM_C_Persona_Nacionalidad] ([TN_ID_Nacionalidad], [TC_Descripcion], [TF_Fecha_Creacion]) VALUES (6, N'Otro o desconocido', CAST(N'2020-11-06T00:00:00.000' AS DateTime))
+INSERT [dbo].[TMCCM_C_Persona_Nacionalidad] ([TN_ID_Nacionalidad], [TC_Descripcion], [TF_Fecha_Creacion]) VALUES (7, N'Costa Rica', CAST(N'2020-11-06T00:00:00.000' AS DateTime))
+INSERT [dbo].[TMCCM_C_Persona_Nacionalidad] ([TN_ID_Nacionalidad], [TC_Descripcion], [TF_Fecha_Creacion]) VALUES (8, N'España', CAST(N'2020-11-06T00:00:00.000' AS DateTime))
+INSERT [dbo].[TMCCM_C_Persona_Nacionalidad] ([TN_ID_Nacionalidad], [TC_Descripcion], [TF_Fecha_Creacion]) VALUES (9, N'Abjasia', CAST(N'2020-11-06T00:00:00.000' AS DateTime))
+SET IDENTITY_INSERT [dbo].[TMCCM_C_Persona_Nacionalidad] OFF
+GO
+
+--C_Person Sexo
+SET IDENTITY_INSERT [dbo].[TMCCM_C_Persona_Sexo] ON 
+
+INSERT [dbo].[TMCCM_C_Persona_Sexo] ([TN_ID_Sexo], [TC_Descripcion], [TB_Eliminado]) VALUES (7, N'Desconocido', 0)
+INSERT [dbo].[TMCCM_C_Persona_Sexo] ([TN_ID_Sexo], [TC_Descripcion], [TB_Eliminado]) VALUES (8, N'Hombre', 0)
+INSERT [dbo].[TMCCM_C_Persona_Sexo] ([TN_ID_Sexo], [TC_Descripcion], [TB_Eliminado]) VALUES (9, N'Mujer', 0)
+SET IDENTITY_INSERT [dbo].[TMCCM_C_Persona_Sexo] OFF
+GO
+
+--C_Persona tipo identificación
+SET IDENTITY_INSERT [dbo].[TMCCM_C_Persona_Tipo_Identificacion] ON 
+
+INSERT [dbo].[TMCCM_C_Persona_Tipo_Identificacion] ([TN_ID_Tipo_Identificacion], [TC_Descripcion], [TC_Mascara], [TF_Fecha_Creacion]) VALUES (7, N'Desconocido', NULL, CAST(N'2020-11-06T00:00:00.000' AS DateTime))
+INSERT [dbo].[TMCCM_C_Persona_Tipo_Identificacion] ([TN_ID_Tipo_Identificacion], [TC_Descripcion], [TC_Mascara], [TF_Fecha_Creacion]) VALUES (8, N'Cedula', NULL, CAST(N'2020-11-06T00:00:00.000' AS DateTime))
+INSERT [dbo].[TMCCM_C_Persona_Tipo_Identificacion] ([TN_ID_Tipo_Identificacion], [TC_Descripcion], [TC_Mascara], [TF_Fecha_Creacion]) VALUES (9, N'Cedula de residencia', NULL, CAST(N'2020-11-06T00:00:00.000' AS DateTime))
+SET IDENTITY_INSERT [dbo].[TMCCM_C_Persona_Tipo_Identificacion] OFF
+GO
+
+--C_Vehiculo Clase
+SET IDENTITY_INSERT [dbo].[TMCCM_C_Vehiculo_Clase] ON 
+
+INSERT [dbo].[TMCCM_C_Vehiculo_Clase] ([TN_ID_Clase_Vehiculo], [TC_Descripcion], [TF_Fecha_Creacion], [TB_Eliminado]) VALUES (10, N'Motocicleta', CAST(N'2020-11-06T00:00:00.000' AS DateTime), 0)
+INSERT [dbo].[TMCCM_C_Vehiculo_Clase] ([TN_ID_Clase_Vehiculo], [TC_Descripcion], [TF_Fecha_Creacion], [TB_Eliminado]) VALUES (11, N'Automovil', CAST(N'2020-11-06T00:00:00.000' AS DateTime), 0)
+INSERT [dbo].[TMCCM_C_Vehiculo_Clase] ([TN_ID_Clase_Vehiculo], [TC_Descripcion], [TF_Fecha_Creacion], [TB_Eliminado]) VALUES (12, N'Motocarro', CAST(N'2020-11-06T00:00:00.000' AS DateTime), 0)
+INSERT [dbo].[TMCCM_C_Vehiculo_Clase] ([TN_ID_Clase_Vehiculo], [TC_Descripcion], [TF_Fecha_Creacion], [TB_Eliminado]) VALUES (13, N'4X4', CAST(N'2020-11-06T00:00:00.000' AS DateTime), 0)
+SET IDENTITY_INSERT [dbo].[TMCCM_C_Vehiculo_Clase] OFF
+GO
+
+--C_Vehiculo Color
+SET IDENTITY_INSERT [dbo].[TMCCM_C_Vehiculo_Color] ON 
+
+INSERT [dbo].[TMCCM_C_Vehiculo_Color] ([TN_ID_Color_Vehiculo], [TC_Descripcion], [TF_Fecha_Creacion], [TB_Eliminado]) VALUES (5, N'Desconocido', CAST(N'2020-11-06T00:00:00.000' AS DateTime), 0)
+INSERT [dbo].[TMCCM_C_Vehiculo_Color] ([TN_ID_Color_Vehiculo], [TC_Descripcion], [TF_Fecha_Creacion], [TB_Eliminado]) VALUES (6, N'Celeste', CAST(N'2020-11-06T00:00:00.000' AS DateTime), 0)
+INSERT [dbo].[TMCCM_C_Vehiculo_Color] ([TN_ID_Color_Vehiculo], [TC_Descripcion], [TF_Fecha_Creacion], [TB_Eliminado]) VALUES (7, N'Blanco', CAST(N'2020-11-06T00:00:00.000' AS DateTime), 0)
+INSERT [dbo].[TMCCM_C_Vehiculo_Color] ([TN_ID_Color_Vehiculo], [TC_Descripcion], [TF_Fecha_Creacion], [TB_Eliminado]) VALUES (8, N'Rojo', CAST(N'2020-11-06T00:00:00.000' AS DateTime), 0)
+SET IDENTITY_INSERT [dbo].[TMCCM_C_Vehiculo_Color] OFF
+GO
+
+--C_Vehiculo Marca
+SET IDENTITY_INSERT [dbo].[TMCCM_C_Vehiculo_Marca] ON 
+
+INSERT [dbo].[TMCCM_C_Vehiculo_Marca] ([TN_ID_Marca_Vehiculo], [TC_Descripcion], [TF_Fecha_Creacion], [TB_Eliminado]) VALUES (7, N'Desconocido', CAST(N'2020-11-06T00:00:00.000' AS DateTime), 0)
+INSERT [dbo].[TMCCM_C_Vehiculo_Marca] ([TN_ID_Marca_Vehiculo], [TC_Descripcion], [TF_Fecha_Creacion], [TB_Eliminado]) VALUES (8, N'Marca no registrada', CAST(N'2020-11-06T00:00:00.000' AS DateTime), 0)
+INSERT [dbo].[TMCCM_C_Vehiculo_Marca] ([TN_ID_Marca_Vehiculo], [TC_Descripcion], [TF_Fecha_Creacion], [TB_Eliminado]) VALUES (9, N'Foredil', CAST(N'2020-11-06T00:00:00.000' AS DateTime), 0)
+INSERT [dbo].[TMCCM_C_Vehiculo_Marca] ([TN_ID_Marca_Vehiculo], [TC_Descripcion], [TF_Fecha_Creacion], [TB_Eliminado]) VALUES (10, N'Toyota', CAST(N'2020-11-06T00:00:00.000' AS DateTime), 0)
+SET IDENTITY_INSERT [dbo].[TMCCM_C_Vehiculo_Marca] OFF
+] ON 
+
+
+--Entidad Persona
+INSERT [dbo].[TMCCM_Entidad_Persona] ([TN_ID_Persona], [TN_ID_Caso], [TN_ID_Tipo_Identificacion], [TN_ID_Sexo], [TN_ID_Genero], [TN_ID_Nacionalidad], [TC_Nombre], [TC_Primer_Apellido], [TC_Segundo_Apellido], [TF_Fecha_Nacimiento], [TN_Edad], [TB_Fotografia], [TC_Cedula], [TB_Fallecido], [TN_Autopsia], [TB_Exp_Criminal], [TF_Fecha_Creacion], [TF_Fecha_Modificacion], [TC_Creado_Por], [TC_Modificado_Por], [TB_Verificado], [TB_Eliminado], [TC_Comentario], [TC_Alias]) VALUES (6, 9, 8, 10, 6, N'Alejandra ', N'Rivera ', N'Castro', CAST(N'1985-05-30' AS Date), 1,null ,N'104900580', 0, NULL, 0, CAST(N'2020-11-06T00:00:00.000' AS DateTime), NULL, N'Valeria Leiva Quirós', NULL, 1, 0, NULL, N'Impostora')
+
+INSERT [dbo].[TMCCM_Entidad_Persona] ([TN_ID_Persona], [TN_ID_Caso], [TN_ID_Tipo_Identificacion], [TN_ID_Sexo], [TN_ID_Genero], [TN_ID_Nacionalidad], [TC_Nombre], [TC_Primer_Apellido], [TC_Segundo_Apellido], [TF_Fecha_Nacimiento], [TN_Edad], [TB_Fotografia], [TC_Cedula], [TB_Fallecido], [TN_Autopsia], [TB_Exp_Criminal], [TF_Fecha_Creacion], [TF_Fecha_Modificacion], [TC_Creado_Por], [TC_Modificado_Por], [TB_Verificado], [TB_Eliminado], [TC_Comentario], [TC_Alias]) VALUES (6, 9, 8, 10, 6, N'Liam ', N'Barquero ', N'Moreira', CAST(N'1992-05-30' AS Date), 1,null ,N'389011300', 0, NULL, 1, CAST(N'2020-11-06T00:00:00.000' AS DateTime), NULL, N'Valeria Leiva Quirós', NULL, 1, 0, NULL, N'Profesor')
+
+
+--Entidad Vehiculo
+INSERT [dbo].[TMCCM_Entidad_Vehiculo] ([TN_ID_Vehiculo], [TN_ID_Caso], [TN_ID_Marca_Vehiculo], [TN_ID_Color_Vehiculo], [TC_Placa], [TC_Estilo], [TC_Comentario], [TN_Anno], [TF_Fecha_Creacion], [TF_Fecha_Modificacion], [TC_Creado_Por], [TC_Modificado_Por], [TB_Eliminado], [TB_Verificado], [TB_Fotografia], [TN_ID_Clase_Vehiculo]) VALUES (50, 6, 7, 5, N'12365', N' ', NULL, NULL, CAST(N'2020-11-06T00:00:00.000' AS DateTime), CAST(N'2020-11-06T00:00:00.000' AS DateTime), N'Valeria Leiva Quirós', N'Valeria Leiva Quirós', 0, 0, NULL, 11)
+
+INSERT [dbo].[TMCCM_Entidad_Vehiculo] ([TN_ID_Vehiculo], [TN_ID_Caso], [TN_ID_Marca_Vehiculo], [TN_ID_Color_Vehiculo], [TC_Placa], [TC_Estilo], [TC_Comentario], [TN_Anno], [TF_Fecha_Creacion], [TF_Fecha_Modificacion], [TC_Creado_Por], [TC_Modificado_Por], [TB_Eliminado], [TB_Verificado], [TB_Fotografia], [TN_ID_Clase_Vehiculo]) VALUES (50, 6, 7, 5, N'33304', N' ', NULL, NULL, CAST(N'2020-11-06T00:00:00.000' AS DateTime), CAST(N'2020-11-06T00:00:00.000' AS DateTime), N'Valeria Leiva Quirós', N'Valeria Leiva Quirós', 0, 0, NULL, 11)
+
